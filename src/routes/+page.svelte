48b686ed303808cacc446a908a78578f07b6eb65
OverflowCat <overflowcat@gmail.com>
2023-12-09 22:11:02 +0800

    feat: working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import type { Config } from "@sveltejs/kit";
+  import type { PageData } from "./$types";
+  export let data: PageData;
+  export const config: Config = {
+    runtime: "edge",
+  };
+</script>
+
+<h1>Biliarchiver Bot Status</h1>
+<p>Running: {data?.archived?.success || "down"}</p>
+<h2>Archived recently</h2>
+<ul>
+  {#each data?.archived?.items || [] as { added_time, bvid, status }}
+    <li>
+      <a class="bvid" href="https://www.bilibili.com/video/{bvid}">{bvid}</a>
+      <time>{added_time}</time>
+      <mark>{status}</mark>
+    </li>
+  {/each}
+</ul>
+
+<style>
+  * {
+    font-family: "Noto Sans CJK SC", "Noto Sans", "HarmonyOS Sans", "Mi Sans",
+      "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei",
+      "WenQuanYi Micro Hei", sans-serif;
+  }
+  .bvid,
+  time {
+    font-family: monospace;
+  }
+</style>
