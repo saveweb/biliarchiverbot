1a57859d35071c743938d466fd6ed3ad6b7b3fdf
Ovler <ovlertheyoung@gmail.com>
2024-03-17 17:35:31 +0800

    Add dotenv dependency and sample .env

--- bot/src/markup.ts
+++ bot/src/markup.ts
@@ -1,10 +1,19 @@
 import { InlineKeyboardMarkup } from "telegraf/types";
+require('dotenv').config()
+const bot_username = process.env.BILIARCHIVER_USERNAME;
+let web_app_url = process.env.BILIARCHIVER_WEBAPP;
+if (!bot_username) {
+  throw new Error("\x1b[31mBILIARCHIVER_USERNAME must be provided!\x1b[0m");
+}
+if (!web_app_url) {
+  web_app_url = "https://biliarchiverbot-miniapp.vercel.app/"
+}
 export const MINIAPP_PRIVATE: InlineKeyboardMarkup = {
     inline_keyboard: [
       [
         {
           text: "View status",
-          web_app: { url: "https://biliarchiverbot-miniapp.vercel.app/" },
+          web_app: { url: web_app_url },
         },
       ],
     ],
@@ -14,7 +23,7 @@ export const MINIAPP_PRIVATE: InlineKeyboardMarkup = {
       [
         {
           text: "Check status",
-          url: "https://t.me/lefetchbot/list"
+          url: `https://t.me/${bot_username}/list`,
         }
       ]
     ]
