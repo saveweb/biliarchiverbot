841ae86288ba67ef77cd0561aa9a15daafbe0ab3
Ovler <ovlertheyoung@gmail.com>
2024-06-16 01:32:22 +0800

    feat: support more

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -29,17 +29,45 @@ const handleBiliLink = async (ctx: Context) => {
   console.info("Resolved", text);
   const matches = /BV[a-zA-Z0-9]+/.exec(text);
   if (!matches) {
-    const listmatches = /\/(play|list)\/ml(\d+)|\/favlist\?fid=(\d+)/.exec(text);
+    // season
+    // https://space.bilibili.com/38505812/channel/collectiondetail?sid=518558 518558
+    // https://space.bilibili.com/228147/favlist?fid=157228&ftype=collect&ctype=21 157228
+    const seasonmatches = /\/collectiondetail\?sid=(\d+)|\/favlist\?fid=(\d+)&ftype=collect&ctype=21/.exec(text);
+    if (seasonmatches) {
+      console.info(seasonmatches);
+      console.info("Season matches", seasonmatches[1] || seasonmatches[2]);
+      handle_source(ctx, "season", seasonmatches[1] || seasonmatches[2]);
+      return;
+    }
+
+    // https://www.bilibili.com/medialist/detail/ml480798947?type=1
+    // https://www.bilibili.com/medialist/play/ml480798947
+    // https://www.bilibili.com/list/ml480798947
+    // https://space.bilibili.com/365743248/favlist?fid=2927461681&ftype=collect&ctype=11
+    const listmatches = /\/(play|list|detail)\/ml(\d+)|\/favlist\?fid=(\d+)/.exec(text);
     if (listmatches) {
       console.info("List matches", listmatches[2] || listmatches[3]);
       handle_source(ctx, "favlist", listmatches[2] || listmatches[3]);
       return;
     }
 
-    const uidmatches = /space\.bilibili\.com\/(\d+)/.exec(text);
+    // series
+    // https://www.bilibili.com/list/617813050?sid=518558&desc=1 518558
+    // https://space.bilibili.com/617813050/channel/seriesdetail?sid=518558&ctype=0 518558 yes
+    const seriesmatches = /\/list\/(\d+)\?sid=(\d+)|com\/(\d+)\/channel\/seriesdetail\?sid=(\d+)/.exec(text);
+    if (seriesmatches) {
+      console.info("Series matches", seriesmatches[2] || seriesmatches[4]);
+      handle_source(ctx, "series", seriesmatches[2] || seriesmatches[4]);
+      return;
+    }
+
+    // up_videos
+    // https://space.bilibili.com/38505812
+    // https://www.bilibili.com/list/228147
+    const uidmatches = /space\.bilibili\.com\/(\d+)|\/list\/(\d+)/.exec(text);
     if (uidmatches) {
-      console.info("UID matches", uidmatches[1]);
-      handle_source(ctx, "up_videos", uidmatches[1]);
+      console.info("UID matches", uidmatches[1] || uidmatches[2]);
+      handle_source(ctx, "up_videos", uidmatches[1] || uidmatches[2]);
       return;
     }
     return;
@@ -120,7 +148,7 @@ const handle_source = async (ctx: Context, source_type: string, source_id: strin
   }
   let pending;
   try {
-    pending = await ctx.reply("Getting items from source……", {
+    pending = await ctx.reply(`Getting items from source ${source_type} ${source_id}……`, {
       reply_to_message_id: ctx.message.message_id,
     });
   } catch (e) {
@@ -141,7 +169,7 @@ const handle_source = async (ctx: Context, source_type: string, source_id: strin
           }
         );
       } else {
-        await ctx.reply(`Archive request failed.`, {
+        await ctx.reply(`Archive request failed for source ${source_type} ${source_id}.`, {
           reply_markup,
         });
         return;
